b1e6b930ab4b0d11981cf8f8278481b3cae44313
Don Aime <118371312+DUSHIME1212@users.noreply.github.com>
2024-01-16 18:05:31 +0200

    Dashboard

---
+++ src/pages/DARSHBOARD.js
@@ -0,0 +1,266 @@
+const THREADS = () => {
+    return (
+      <div className="relative bg-black w-full overflow-hidden flex flex-col items-center justify-start pt-0 px-0 pb-[220px] box-border gap-[36px] tracking-[normal] text-left text-xs text-gray-300 font-inter mq725:gap-[18px]">
+        <header className="self-stretch overflow-hidden flex flex-row items-start justify-between py-[23px] px-16 gap-[20px] mq725:pl-8 mq725:pr-8 mq725:box-border">
+          <img
+            className="relative w-6 h-6 overflow-hidden shrink-0"
+            loading="eager"
+            alt=""
+            src="/mingcutethreadsline.svg"
+          />
+          <div className="overflow-hidden flex flex-row items-start justify-start py-2.5 pr-[22px] pl-5 gap-[30px] mq450:gap-[15px]">
+            <img
+              className="relative w-6 h-6 overflow-hidden shrink-0 min-h-[24px]"
+              loading="eager"
+              alt=""
+              src="/home05.svg"
+            />
+            <img
+              className="relative w-6 h-6 overflow-hidden shrink-0 min-h-[24px]"
+              loading="eager"
+              alt=""
+              src="/searchlg.svg"
+            />
+            <img
+              className="relative w-6 h-6 overflow-hidden shrink-0 min-h-[24px]"
+              loading="eager"
+              alt=""
+              src="/edit05.svg"
+            />
+            <img
+              className="relative w-6 h-6 overflow-hidden shrink-0 min-h-[24px]"
+              loading="eager"
+              alt=""
+              src="/heartrounded.svg"
+            />
+            <img
+              className="relative w-6 h-6 overflow-hidden shrink-0 min-h-[24px]"
+              loading="eager"
+              alt=""
+              src="/user01.svg"
+            />
+          </div>
+          <img
+            className="relative w-6 h-6 overflow-hidden shrink-0"
+            loading="eager"
+            alt=""
+            src="/menu04.svg"
+          />
+        </header>
+        <div className="rounded-[40px] bg-gray-200 w-[480px] flex flex-row items-center justify-between p-2.5 box-border gap-[20px] max-w-full mq650:w-[calc(100%_-_40px)] mq450:flex-wrap">
+          <div className="flex flex-row items-center justify-start gap-[15px]">
+            <img
+              className="relative rounded-[30px] w-[50px] h-[50px] object-cover"
+              loading="eager"
+              alt=""
+              src="/rectangle-1@2x.png"
+            />
+            <div className="relative capitalize">start a thread...</div>
+          </div>
+          <button className="cursor-pointer [border:none] p-2.5 bg-gray-100 rounded-xl flex flex-row items-center justify-center hover:bg-gainsboro">
+            <div className="relative text-xs capitalize font-inter text-black text-left">
+              post
+            </div>
+          </button>
+        </div>
+        <div className="w-[520px] flex flex-col items-start justify-start py-0 px-5 box-border gap-[15px] max-w-full text-sm-7 text-white font-manrope">
+          <div className="self-stretch box-border overflow-hidden flex flex-col items-start justify-start pt-[23px] pb-[42px] pr-8 pl-[26px] gap-[27px] max-w-full border-b-[1px] border-solid border-gray-400 mq650:pt-5 mq650:pb-[27px] mq650:box-border">
+            <div className="self-stretch flex flex-row items-center justify-between gap-[20px] mq450:flex-wrap">
+              <div className="flex flex-row items-center justify-start gap-[17px]">
+                <img
+                  className="relative rounded-[50%] w-[50px] h-[50px] object-cover"
+                  loading="eager"
+                  alt=""
+                  src="/ellipse-1@2x.png"
+                />
+                <div className="flex flex-col items-start justify-start">
+                  <div className="relative capitalize">Google</div>
+                  <div className="relative text-2xs-3 capitalize text-gray-300">
+                    Fancy seeing you here, Bugdroids
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-start gap-[15px] text-2xs-3 text-gray-300">
+                <div className="relative capitalize">1d</div>
+                <img
+                  className="relative w-[17px] h-[17px] overflow-hidden shrink-0 min-h-[17px]"
+                  alt=""
+                  src="/tablerdots.svg"
+                />
+              </div>
+            </div>
+            <div className="w-[335px] flex flex-row items-start justify-start py-0 px-[13px] box-border max-w-full text-2xs-3 text-gray-300">
+              <div className="flex-1 flex flex-col items-start justify-start gap-[9px]">
+                <div className="self-stretch flex flex-row items-start justify-start py-0 pr-0 pl-2">
+                  <div className="flex-1 flex flex-row flex-wrap items-start justify-start gap-[21px]">
+                    <div className="relative box-border w-px h-[371px] border-r-[1px] border-solid border-gray-300 mq450:w-full mq450:h-px mq450:box-border mq450:border-t-[1px] mq450:border-solid mq450:border-gray-300" />
+                    <div className="flex-1 flex flex-col items-start justify-start gap-[23px] min-w-[182px] shrink-0">
+                      <img
+                        className="self-stretch relative rounded-xl max-w-full overflow-hidden h-[327px] shrink-0 object-cover"
+                        loading="eager"
+                        alt=""
+                        src="/rectangle-2@2x.png"
+                      />
+                      <div className="flex flex-row items-start justify-start py-0 pr-px pl-0 gap-[20px]">
+                        <img
+                          className="relative w-[20.3px] h-[20.3px] overflow-hidden shrink-0 min-h-[20px]"
+                          loading="eager"
+                          alt=""
+                          src="/heartrounded-1.svg"
+                        />
+                        <img
+                          className="relative w-[20.3px] h-[20.3px] overflow-hidden shrink-0 min-h-[20px]"
+                          loading="eager"
+                          alt=""
+                          src="/messagecircle01.svg"
+                        />
+                        <img
+                          className="relative w-[20.3px] h-[20.3px] overflow-hidden shrink-0 min-h-[20px]"
+                          loading="eager"
+                          alt=""
+                          src="/refreshccw02.svg"
+                        />
+                        <img
+                          className="relative w-[20.3px] h-[20.3px] overflow-hidden shrink-0 min-h-[20px]"
+                          loading="eager"
+                          alt=""
+                          src="/share06.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-row items-end justify-start gap-[5px]">
+                  <div className="relative w-[34px] h-[25px]">
+                    <img
+                      className="absolute top-[10px] left-[0px] rounded-[50%] w-2.5 h-2.5 object-cover"
+                      loading="eager"
+                      alt=""
+                      src="/ellipse-2@2x.png"
+                    />
+                    <img
+                      className="absolute top-[18px] left-[10px] rounded-[50%] w-[7px] h-[7px] object-cover z-[1]"
+                      loading="eager"
+                      alt=""
+                      src="/ellipse-4@2x.png"
+                    />
+                    <img
+                      className="absolute top-[0px] left-[11px] rounded-[50%] w-[17px] h-[17px] object-cover"
+                      loading="eager"
+                      alt=""
+                      src="/ellipse-3@2x.png"
+                    />
+                  </div>
+                  <div className="relative capitalize">42 replies</div>
+                  <div className="flex flex-row items-start justify-start gap-[5px]">
+                    <div className="relative capitalize">.</div>
+                    <div className="relative capitalize">view more activites</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch box-border overflow-hidden flex flex-col items-start justify-start pt-[23px] pb-[42px] pr-8 pl-[26px] gap-[27px] max-w-full border-b-[1px] border-solid border-gray-400 mq650:pt-5 mq650:pb-[27px] mq650:box-border">
+            <div className="self-stretch flex flex-row items-center justify-between gap-[20px] mq450:flex-wrap">
+              <div className="flex flex-row items-center justify-start gap-[17px]">
+                <img
+                  className="relative rounded-[50%] w-[50px] h-[50px] object-cover"
+                  loading="eager"
+                  alt=""
+                  src="/ellipse-1-1@2x.png"
+                />
+                <div className="flex flex-col items-start justify-start">
+                  <div className="relative capitalize">Google</div>
+                  <div className="relative text-2xs-3 capitalize text-gray-300">
+                    Fancy seeing you here, Bugdroids
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-start gap-[15px] text-2xs-3 text-gray-300">
+                <div className="relative capitalize">1d</div>
+                <img
+                  className="relative w-[17px] h-[17px] overflow-hidden shrink-0 min-h-[17px]"
+                  loading="eager"
+                  alt=""
+                  src="/tablerdots-1.svg"
+                />
+              </div>
+            </div>
+            <div className="w-[335px] flex flex-row items-start justify-start py-0 px-[13px] box-border max-w-full text-2xs-3 text-gray-300">
+              <div className="flex-1 flex flex-col items-start justify-start gap-[9px]">
+                <div className="self-stretch flex flex-row items-start justify-start py-0 pr-0 pl-2">
+                  <div className="flex-1 flex flex-row flex-wrap items-start justify-start gap-[21px]">
+                    <div className="relative box-border w-px h-[371px] border-r-[1px] border-solid border-gray-300 mq450:w-full mq450:h-px mq450:box-border mq450:border-t-[1px] mq450:border-solid mq450:border-gray-300" />
+                    <div className="flex-1 flex flex-col items-start justify-start gap-[23px] min-w-[182px] shrink-0">
+                      <img
+                        className="self-stretch relative rounded-xl max-w-full overflow-hidden h-[327px] shrink-0 object-cover"
+                        loading="eager"
+                        alt=""
+                        src="/rectangle-2-1@2x.png"
+                      />
+                      <div className="flex flex-row items-start justify-start py-0 pr-px pl-0 gap-[20px]">
+                        <img
+                          className="relative w-[20.3px] h-[20.3px] overflow-hidden shrink-0 min-h-[20px]"
+                          loading="eager"
+                          alt=""
+                          src="/heartrounded-2.svg"
+                        />
+                        <img
+                          className="relative w-[20.3px] h-[20.3px] overflow-hidden shrink-0 min-h-[20px]"
+                          loading="eager"
+                          alt=""
+                          src="/messagecircle01-1.svg"
+                        />
+                        <img
+                          className="relative w-[20.3px] h-[20.3px] overflow-hidden shrink-0 min-h-[20px]"
+                          loading="eager"
+                          alt=""
+                          src="/refreshccw02-1.svg"
+                        />
+                        <img
+                          className="relative w-[20.3px] h-[20.3px] overflow-hidden shrink-0 min-h-[20px]"
+                          loading="eager"
+                          alt=""
+                          src="/share06-1.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-row items-end justify-start gap-[5px]">
+                  <div className="relative w-[34px] h-[25px]">
+                    <img
+                      className="absolute top-[10px] left-[0px] rounded-[50%] w-2.5 h-2.5 object-cover"
+                      loading="eager"
+                      alt=""
+                      src="/ellipse-2-1@2x.png"
+                    />
+                    <img
+                      className="absolute top-[18px] left-[10px] rounded-[50%] w-[7px] h-[7px] object-cover z-[1]"
+                      loading="eager"
+                      alt=""
+                      src="/ellipse-4-1@2x.png"
+                    />
+                    <img
+                      className="absolute top-[0px] left-[11px] rounded-[50%] w-[17px] h-[17px] object-cover"
+                      loading="eager"
+                      alt=""
+                      src="/ellipse-3-1@2x.png"
+                    />
+                  </div>
+                  <div className="relative capitalize">42 replies</div>
+                  <div className="flex flex-row items-start justify-start gap-[5px]">
+                    <div className="relative capitalize">.</div>
+                    <div className="relative capitalize">view more activites</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default THREADS;
+  
